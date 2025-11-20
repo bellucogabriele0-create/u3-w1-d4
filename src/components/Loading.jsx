54e4ef60f4cbdc084934b3c01cc2fb0550b6913a
bellucogabriele0-create <bellucogabriele0@gmail.com>
2025-11-20 17:44:05 +0100

    spinner rosso

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loading = () => (
   <div className="text-center my-3">
-    <Spinner animation="border" variant="success" role="status">
+    <Spinner animation="border" variant="danger" role="status">
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   </div>
